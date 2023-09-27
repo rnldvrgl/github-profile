c0ab8bec32a775575c2a7bdcb991b47e608483f0
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-27 17:59:20 +0800

    update

--- app/page.js
+++ app/page.js
@@ -27,6 +27,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { AiFillStar, AiFillEye } from "react-icons/ai";
 import { BsFillPersonFill, BsFillBuildingFill, BsPersonFillCheck } from "react-icons/bs";
+import { RiGitRepositoryFill } from "react-icons/ri";
+import { VscRepoForked } from "react-icons/vsc";
 import { Skeleton } from "@/components/ui/skeleton";
 
 const formSchema = z.object({
@@ -62,14 +64,16 @@ export default function Home() {
     setIsLoading(false);
   };
 
-
   return (
-    <main className="relative flex flex-col items-center justify-start w-full h-full min-h-screen p-6 sm:p-12 gap-y-6 md:p-20 lg:p-24 dark:bg-[#1F242A]">
+    <main className="relative flex flex-col items-center justify-start w-full h-full min-h-screen p-6 sm:p-12 gap-y-6 md:p-20 lg:p-24">
       <nav className="absolute flex items-center justify-between w-full px-6 top-4">
         <ModeToggle />
       </nav>
       <section className="flex flex-col items-center justify-between gap-y-6">
-        <h1 className="max-w-xl mb-6 text-5xl font-bold tracking-wide uppercase text-primary">GitHub Profile</h1>
+        <section className='flex flex-col items-center justify-center'>
+          <h1 className="mb-6 text-5xl font-bold tracking-wide uppercase text-primary">GitHub Profile Viewer</h1>
+          <h4 className='font-normal'>Created By: Ronald Vergel Dela Cruz</h4>
+        </section>
         <Form {...form} className="flex flex-col items-center justify-center gap-y-6">
           <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col items-center justify-center gap-y-6">
             <FormField
@@ -81,9 +85,10 @@ export default function Home() {
                     <Input
                       {...field}
                       placeholder="Search a GitHub user"
+                      className="dark:text-white" // Adjust text color for dark mode
                     />
                   </FormControl>
-                  <FormMessage />
+                  <FormMessage className="dark:text-rose-200" />
                 </FormItem>
               )}
             />
@@ -132,7 +137,7 @@ export default function Home() {
             <div>No user found</div>
           ) : (
             userData ? (
-              <Card className="max-w-[1000px] w-full">
+              <Card className="max-w-[1000px] w-full bg-slate-100 dark:bg-[#4c2885] rounded-[20px]">
                 <CardHeader className="gap-6">
                   <div className="flex flex-col items-center gap-6 lg:flex-row">
                     <Avatar className="border-[10px] border-[#2a2a72] w-[150px] h-[150px]">
@@ -146,7 +151,7 @@ export default function Home() {
                       <CardTitle className="text-center lg:text-left">
                         {userData?.name ? userData.name : `(No Name Available)`}
                       </CardTitle>
-                      <CardDescription className="text-center lg:text-left">
+                      <CardDescription className="text-center lg:text-left dark:text-white">
                         {userData?.bio}
                       </CardDescription>
                       {userData.company && (
@@ -155,9 +160,9 @@ export default function Home() {
                         </div>
                       )}
                       <div className="flex items-center justify-center mt-4 lg:items-start lg:justify-start gap-x-2">
-                        <Badge className="flex gap-1"><BsFillPersonFill /> {userData.followers} Followers</Badge>
-                        <Badge className="flex gap-1"><BsPersonFillCheck /> {userData.following} Following</Badge>
-                        <Badge>{userData.public_repos} Public Repositories</Badge>
+                        <Badge className="flex gap-1 bg-blue-500 dark:bg-blue-300 dark:text-ghdarkgray"><BsFillPersonFill /> {userData.followers} Followers</Badge>
+                        <Badge className="flex gap-1 bg-green-500 dark:bg-green-300 dark:text-ghdarkgray"><BsPersonFillCheck /> {userData.following} Following</Badge>
+                        <Badge className="flex gap-1 bg-yellow-500 dark:bg-yellow-300 dark:text-ghdarkgray"><RiGitRepositoryFill /> {userData.public_repos} Public Repositories</Badge>
                       </div>
                     </div>
                   </div>
@@ -166,27 +171,31 @@ export default function Home() {
             ) : null
           )}
           {repoData.length > 0 && (
-            <Card className="w-full max-w-[1000px]">
+            <Card className="w-full max-w-[1000px] bg-slate-100 dark:bg-[#4c2885] rounded-[20px]">
               <CardHeader>
                 <CardTitle>Public Repositories</CardTitle>
               </CardHeader>
-              <CardContent className="grid grid-cols-1 gap-4 md:grid-cols-2">
+              <CardContent className="grid grid-cols-1 gap-6 md:grid-cols-2">
                 {repoData.map((repo) => (
                   <a target="_blank" href={repo.html_url} key={repo.id}>
-                    <Card className="mb-4">
+                    <Card className="h-full mb-4 bg-slate-300 dark:bg-[#2a2a72] border-0">
                       <CardHeader>
                         <CardTitle>{repo.name}</CardTitle>
                       </CardHeader>
                       <CardContent>
                         <div className="flex items-center mt-2 gap-x-2">
                           {repo.language && <Badge>{repo.language}</Badge>}
-                          <Badge className="flex gap-1 bg-yellow-300 dark:bg-yellow-500 dark:text-white text-ghdarkgray">
+                          <Badge className="flex gap-1 text-white bg-yellow-600 dark:bg-yellow-300 dark:text-ghdarkgray">
                             <AiFillStar />
                             {repo.stargazers_count}
                           </Badge>
-                          <Badge className="flex gap-1">
+                          <Badge className="flex gap-1 bg-gray-800 dark:bg-gray-200">
                             <AiFillEye className="w-4 h-4" />
                             {repo.watchers}
+                          </Badge>
+                          <Badge className="flex gap-1 bg-blue-800 dark:bg-blue-200">
+                            <VscRepoForked className="w-4 h-4" />
+                            {repo.forks_count}
                           </Badge>
                         </div>
                       </CardContent>
