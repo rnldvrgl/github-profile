167c6f7c00a50040b9fc3db1c81728309b0ce847
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-27 16:39:37 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1,20 +1,32 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { fetchUserData } from "@/utils/githubAPI";
-import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/card";
 import { Avatar } from "@/components/ui/avatar";
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { PersonIcon, StarFilledIcon } from "@radix-ui/react-icons";
-import { Backpack, Building, Eye, Forklift } from "lucide-react";
+import { AiFillStar, AiFillEye } from "react-icons/ai";
+import { BsFillPersonFill, BsFillBuildingFill, BsPersonFillCheck } from "react-icons/bs";
 import { Skeleton } from "@/components/ui/skeleton";
 
 const formSchema = z.object({
@@ -50,8 +62,6 @@ export default function Home() {
     setIsLoading(false);
   };
 
-
-  console.log(userData)
   return (
     <main className="relative flex flex-col items-center justify-start w-full h-full min-h-screen p-6 sm:p-12 gap-y-6 md:p-20 lg:p-24 dark:bg-[#1F242A]">
       <nav className="absolute flex items-center justify-between w-full px-6 top-4">
@@ -59,8 +69,8 @@ export default function Home() {
       </nav>
       <section className="flex flex-col items-center justify-between gap-y-6">
         <h1 className="max-w-xl mb-6 text-5xl font-bold tracking-wide uppercase text-primary">GitHub Profile</h1>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col items-center justify-center gap-y-6">
+        <Form {...form} className="flex flex-col items-center justify-center gap-y-6">
+          <form onSubmit={onSubmit} className="flex flex-col items-center justify-center gap-y-6">
             <FormField
               control={form.control}
               name="search"
@@ -83,19 +93,14 @@ export default function Home() {
       {isLoading ? (
         <>
           <div className="flex flex-col items-center gap-6 lg:flex-row">
-            {/* User Avatar Skeleton */}
             <Skeleton className="w-20 h-20 rounded-full" />
             <div className="flex flex-col gap-2">
-              {/* User Name Skeleton */}
               <Skeleton className="h-6 w-[250px]" />
-              {/* User Bio Skeleton */}
               <Skeleton className="h-4 w-[300px]" />
-              {/* Company Badge Skeleton */}
               <div className="flex items-center gap-1">
                 <Skeleton className="w-16 h-4" />
               </div>
-              {/* Followers/Following/Repos Skeleton */}
-              <div className="flex items-center justify-center gap-x-2">
+              <div className="flex items-center justify-center mt-4 lg:items-start lg:justify-start gap-x-2">
                 <Skeleton className="w-20 h-4" />
                 <Skeleton className="w-20 h-4" />
                 <Skeleton className="w-20 h-4" />
@@ -103,15 +108,12 @@ export default function Home() {
             </div>
           </div>
           <div>
-            {/* Repos skeleton */}
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
               {Array(4).fill().map((_, index) => (
                 <div key={index}>
                   <Skeleton className="h-32" />
                   <div className="mt-2">
-                    {/* Repo Name Skeleton */}
                     <Skeleton className="h-4 w-[200px]" />
-                    {/* Repo Info Skeleton */}
                     <div className="flex items-center mt-1 gap-x-2">
                       <Skeleton className="w-12 h-4" />
                       <Skeleton className="w-20 h-4" />
@@ -148,12 +150,12 @@ export default function Home() {
                       </CardDescription>
                       {userData.company && (
                         <div className="flex items-center justify-center mt-4 lg:items-start lg:justify-start gap-x-2">
-                          <Badge className="flex gap-1 py-1" variant="secondary"><Building /> {userData.company}</Badge>
+                          <Badge className="flex gap-1 py-1" variant="secondary"><BsFillBuildingFill /> {userData.company}</Badge>
                         </div>
                       )}
                       <div className="flex items-center justify-center mt-4 lg:items-start lg:justify-start gap-x-2">
-                        <Badge className="flex gap-1"><PersonIcon /> {userData.followers} Followers</Badge>
-                        <Badge>{userData.following} Following</Badge>
+                        <Badge className="flex gap-1"><BsFillPersonFill /> {userData.followers} Followers</Badge>
+                        <Badge className="flex gap-1"><BsPersonFillCheck /> {userData.following} Following</Badge>
                         <Badge>{userData.public_repos} Public Repositories</Badge>
                       </div>
                     </div>
@@ -178,11 +180,11 @@ export default function Home() {
                         <div className="flex items-center mt-2 gap-x-2">
                           {repo.language && <Badge>{repo.language}</Badge>}
                           <Badge className="flex gap-1 bg-yellow-300 dark:bg-yellow-500 dark:text-white text-ghdarkgray">
-                            <StarFilledIcon />
+                            <AiFillStar />
                             {repo.stargazers_count}
                           </Badge>
                           <Badge className="flex gap-1">
-                            <Eye className="w-4 h-4" />
+                            <AiFillEye className="w-4 h-4" />
                             {repo.watchers}
                           </Badge>
                         </div>
